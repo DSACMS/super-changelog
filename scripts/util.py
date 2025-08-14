95cdeb0ca9bd218cc843a0d1d971209847a4c8e9
Isaac Milarsky <imilarsky@gmail.com>
2025-08-14 16:59:08 -0500

    date format
    Signed-off-by: Isaac Milarsky <imilarsky@gmail.com>

--- scripts/util.py
+++ scripts/util.py
@@ -118,7 +118,7 @@ class ChangelogGenerator:
             try:
                 for pr in repo.get_pulls(state="all"):
                     print(pr.created_at)
-                    print(pr.updated_at)
+                    print(self.start_date)
                     if pr.created_at >= self.start_date or pr.updated_at >= self.start_date:
                         repo_data["pulls"].append({
                             "title": pr.title,
