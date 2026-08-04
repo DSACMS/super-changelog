eef028b17940b6b5341544e02b6fd9419c558969
zionmich <113551695+zionmich@users.noreply.github.com>
2026-08-04 13:03:50 -0400

    Restore org_names to include DSACMS and CMS-Enterprise

--- scripts/generate_changelog_weekly.py
+++ scripts/generate_changelog_weekly.py
@@ -21,8 +21,7 @@ def main():
     if not token:
         raise ValueError("Github token not found in environmental variables")
     
-    #org_names = ["DSACMS", "CMS-Enterprise"]
-    org_names = ["natalialuzuriaga-testing-org"]
+    org_names = ["DSACMS", "CMS-Enterprise"]
 
     gen = ChangelogGenerator(token, filename=filename, log_history_start=start_date, log_history_end=end_date)
 
